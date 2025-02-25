16693ead7eb05119a33cde14a7421a37db6ca096
Uguls <chobozaex@gmail.com>
2025-02-25 12:52:43 +0900

    클래스 구현 완

---
+++ src/Calculator.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+    private int result;
+    private double result2;
+
+    private List<String> resultList = new ArrayList<>();
+
+    public int add(int x, int y) {
+        result = x + y;
+        resultList.add(Integer.toString(result));
+        return result;
+    }
+
+    public int sub (int x, int y) {
+        result = x - y;
+        resultList.add(Integer.toString(result));
+        return result;
+    }
+
+    public int mul(int x, int y) {
+        result = x * y;
+        resultList.add(Integer.toString(result));
+        return result;
+    }
+
+    public double div(int x, int y) {
+        result2 = (double) x / y;
+        resultList.add(Double.toString(result2));
+        return result2;
+    }
+
+    public void delResult() {
+        if (!resultList.isEmpty()) {
+            resultList.remove(0);
+        }
+        System.out.println("계산 결과가 저장되어 있지 않습니다.");
+    }
+
+    // Getter, Setter
+    public List<String> getResultList() {
+        return resultList;
+    }
+
+    public void setResultList(List<String> resultList) {
+        this.resultList = resultList;
+    }
+
+}
