38392ab42dbd6ca285a8d9235ab95ee63cf8cdb8
Uguls <chobozaex@gmail.com>
2025-02-26 15:54:21 +0900

    while문을 통해 잘못된 숫자 또는 연산기호를 입력받았을 시 처음으로 돌아가는것이 아닌 해당 부분만 반복하도록 수정

--- src/Main.java
+++ src/Main.java
@@ -10,27 +10,40 @@ public class Main {
 
         while (true) {
             OperatorType type = null;
-            double result = 0;
+            double first, second;
+            String symbol;
 
-            System.out.print("첫 번째 숫자를 입력해 주세요(양의 정수(0포함)): ");
-            double first = sc.nextDouble();
-//            int first2 = sc.nextInt();
+            while (true) {
+                System.out.print("첫 번째 숫자를 입력해 주세요(양의 정수(0포함)): ");
+                first = sc.nextDouble();
+                if (first < 0) {
+                    System.out.println("양의 정수를 입력해 주세요");
+                    continue;
+                } else {
+                    break;
+                }
+            }
 
-            if (first < 0) {
-                System.out.println("양의 정수를 입력해 주세요");
-                continue;
+            while (true) {
+                System.out.print("두 번째 숫자를 입력해 주세요(양의 정수(0포함)): ");
+                second = sc.nextDouble();
+                if (second < 0) {
+                    System.out.println("양의 정수를 입력해 주세요");
+                    continue;
+                } else {
+                    break;
+                }
             }
-            System.out.print("두 번째 숫자를 입력해 주세요(양의 정수(0포함)): ");
-            double second = sc.nextDouble();
-            if (second < 0) {
-                System.out.println("양의 정수를 입력해 주세요");
-                continue;
-            }
-            System.out.print("연산 기호를 입력해 주세요(+,-,x,/): ");
-            String symbol = sc.next();
-            if (symbol.equals("/") && second == 0) {
-                System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                continue;
+
+            while (true){
+                System.out.print("연산 기호를 입력해 주세요(+,-,x,/): ");
+                symbol = sc.next();
+                if (symbol.equals("/") && second == 0) {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    continue;
+                } else {
+                    break;
+                }
             }
 
             switch (symbol) {
@@ -51,7 +64,7 @@ public class Main {
                     continue;
             }
 
-            result = cal.calculate(type, first, second);
+            double result = cal.calculate(type, first, second);
 
             // 계산 결과 및 저장된 계산 기록 출력
             System.out.println("계산 결과는 " + result + "입니다.");
@@ -72,6 +85,7 @@ public class Main {
             // 계산기 종료
             System.out.println("계산기를 종료하시겠습니까?(종료를 원하시면 yes를 입력해주세요)");
             String off = sc.next().toLowerCase(); // toLowerCase를 사용하여 Exit eXit exIt 등 다양한 exit에 대응하도록 함
+
             if ("yes".equals(off)) {
                 break;
             }
