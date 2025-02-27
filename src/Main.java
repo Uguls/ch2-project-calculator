91c67c4f5888e88eddf349b14a86092e6bea03f9
Uguls <chobozaex@gmail.com>
2025-02-27 17:36:39 +0900

    enum 적극 활용 및 소숫점 제한

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -31,56 +32,53 @@ public class Main {
                 }
             }
 
+            // 연사자 입력 받기
+            boolean isOperatorWrong = true; // isOperatorWrong변수를 통해 while문 통제
+            while (isOperatorWrong){
+                System.out.print("연산 기호를 입력해 주세요(+,-,x,/): ");
+                symbol = sc.next();
+
+                // enum OpertorType에서 관리하는 연산자들과 입력받은 symbol을 비교
+                for (OperatorType op : OperatorType.values()) {
+                    if (op.getOperator().equals(symbol)) { // symbol이 null일 가능성이 존재함으로 op와 비교
+                        type = op;
+                        isOperatorWrong = false; // while문을 탈출하기 위해 isOperatorWrong을 false로 설정
+                        break; // 입력받은 연산자가 enum으로 관리하는 연산자중에 있다면 탈출
+                    } else {
+                        System.out.println("잘못된 연산 기호 입니다. 다시 입력해 주세요");
+                        break;
+                    }
+                }
+            }
+
             while (true) {
                 try {
-                    System.out.print("두 번째 숫자를 입력해 주세요(양의 정수(0포함)): ");
+                    System.out.print("두 번째 숫자를 입력해 주세요(양의 정수(0포함), 나눗셈의 경우 0 입력 불가): ");
                     second = sc.nextDouble();
-                    System.out.println(second);
+
                     if (second < 0) {
                         System.out.println("양의 정수를 입력해 주세요");
+                        continue; // 다시 입력 받도록 continue
+                    } else if ("/".equals(symbol) && second == 0) {
+                        System.out.println("나눗셈의 경우 0은 입력할 수 없습니다.");
                         continue;
-                    }
-                    break;
+                    } else {
+                        break; // 정상적으로 입력 받았다면 break를 사용하여 while 탈출
+                    } 
+                    
                 } catch (InputMismatchException e) {
                     System.out.println("잘못된 입력입니다. 숫자를 입력해주세요");
                     sc.nextLine();
                 }
             }
 
-            while (true){
-                System.out.print("연산 기호를 입력해 주세요(+,-,x,/): ");
-                symbol = sc.next();
-                if (symbol.equals("/") && second == 0) {
-                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                    continue;
-                } else {
-                    break;
-                }
-            }
-
-            switch (symbol) {
-                case "+" :
-                    type = OperatorType.ADD;
-                    break;
-                case "-" :
-                    type = OperatorType.SUB;
-                    break;
-                case "x" :
-                    type = OperatorType.MUL;
-                    break;
-                case "/" :
-                    type = OperatorType.DIV;
-                    break;
-                default:
-                    System.out.println("잘못된 연산 기호 입니다. 다시 입력해 주세요");
-                    continue;
-            }
-
-            double result = cal.calculate(type, first, second);
+            // 소숫점 3자리까지 제한
+            DecimalFormat decimalFormat = new DecimalFormat("#.###");
+            String result = decimalFormat.format(cal.calculate(type, first, second));
 
             // 계산 결과 및 저장된 계산 기록 출력
             System.out.println("계산 결과는 " + result + "입니다.");
-            System.out.println(cal.getCount() + " 번 계산하셨습니다.");
+            System.out.println((int)cal.getCount() + "번 계산하셨습니다.");
             System.out.println("계산 기록 : " + cal.getResultList());
             
             // 가장 오래된 기록 삭제
